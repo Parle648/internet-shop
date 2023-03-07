e4a2d993cf266b3b982c2776f7b585f1303fac48
Parle648 <sashahoumi@gmail.com>
2023-03-07 16:15:05 +0200

    init: make catalog page + sort

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-const arr = [1, 2, 3, 5, 69, [2], [342]]
+const filterHeader = document.querySelector('.filter__header');
+const filterBody = document.querySelector('.filter__body');
+let biggest = document.querySelector('#biggest');
+const carts = document.querySelector('.product-wrapper');
 
-console.log(arr.some((item) => item == '69'))
-console.log(arr.reduce((result, item) => result + item))
-console.log(arr.every((item) => item == '69'))
-console.log(arr.flat())
+filterHeader.addEventListener('click', toggleSelect)
+filterBody.addEventListener('click', sortCarts)
 
-const block = document.querySelector('.flex')
-const btn = document.querySelector('.up')
-const btn2 = document.querySelector('.down')
-
-btn.onclick = sortUp;
-btn2.onclick = sortDown;
-
-function sortUp(){
-    const arr = [...block.children]
-    arr.sort((a, b) => {
-        console.log(a.dataset.cost)
-        return a.dataset.cost - b.dataset.cost
-    })
-    block.innerHTML = ''
-
-    for (let div of arr){
-        block.appendChild(div)
+function toggleSelect(event){
+    filterBody.classList.toggle('filter__body_active')
+}
+function sortCarts(event){
+    if(event.currentTarget.firstElementChild.getAttribute('id') === 'lower'){
+        let arraySort = [...carts.children].sort((a, b) => {
+            return +a.querySelector('.prod__real-cost').innerText.split(' ')[0] - (+b.querySelector('.prod__real-cost').innerText.split(' ')[0])
+        })
+        carts.innerHTML = ''
+        for(let elem of arraySort){
+            carts.appendChild(elem)
+        }
+        changeFilter('biggest')
+        toggleSelect()
+    } else {
+        let arraySort = [...carts.children].sort((a, b) => {
+            return +b.querySelector('.prod__real-cost').innerText.split(' ')[0] - (+a.querySelector('.prod__real-cost').innerText.split(' ')[0])
+        })
+        carts.innerHTML = ''
+        for(let elem of arraySort){
+            carts.appendChild(elem)
+        }
+        changeFilter('lower')
+        toggleSelect()
     }
 }
+function changeFilter(value){
+    filterBody.firstElementChild.setAttribute('id', value)
+    buffer = filterBody.firstElementChild.innerHTML
+    filterBody.firstElementChild.innerHTML = filterHeader.firstElementChild.innerHTML
+    filterHeader.firstElementChild.innerHTML = buffer
+}
 
-function sortDown(){
-    const arr = [...block.children]
-    arr.sort((a, b) => {
-        console.log(a.dataset.cost)
-        return b.dataset.cost - a.dataset.cost
-    })
-    block.innerHTML = ''
+const selectHeader = document.querySelectorAll('.cost-select__header');
 
-    for (let div of arr){
-        block.appendChild(div)
-    }
+selectHeader.forEach((header) => {
+    header.addEventListener('click', toggleNextElem)
+})
+
+function toggleNextElem(event){
+    event.currentTarget.nextElementSibling.classList.toggle('cost-select__body_active')
+    event.currentTarget.lastElementChild.classList.toggle('select__arrow_active')
 }
